aee2dc68398303ee88579a36850e40fea9f95c4f
Dan Korkelia <mail@dankorkelia.com>
2018-01-04 15:34:52 +0000

    Initial Commit

---
+++ Shuffle Array/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shuffle Array
+//
+//  Created by Dan Korkelia on 04/01/2018.
+//  Copyright © 2018 Ramdan Korkelia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
